a94f99fd87ef4c9a87fd914eea3d01e21a5011fa
carloshenrique-dev <c.tarabal25@gmail.com>
2025-04-05 13:06:44 -0300

    refactor(project_mark): Service fix

--- lib/src/constants/weather_service_constants.dart
+++ lib/src/constants/weather_service_constants.dart
@@ -1,4 +1,4 @@
 sealed class WeatherServiceConstants {
   static const String apiKey = 'e761c30759114a6a85f234313250404';
-  static const String baseUrl = 'https://api.weatherapi.com/v1';
+  static const String baseUrl = 'http://api.weatherapi.com/v1';
 }
