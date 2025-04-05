649182389a1a0b88e12192cff8d8fb6f789082c4
carloshenrique-dev <c.tarabal25@gmail.com>
2025-04-05 12:37:24 -0300

    test(project_mark): Fix unit tests

--- test/src/data/services/weather_service_test.dart
+++ test/src/data/services/weather_service_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
 import 'package:project_mark/src/constants/weather_service_constants.dart';
-import 'package:project_mark/src/data/models/city_weather.dart';
 import 'package:project_mark/src/data/models/weather_response.dart';
 import 'package:project_mark/src/data/services/weather_service.dart';
 
@@ -21,27 +20,31 @@ void main() {
     weatherService = WeatherService(client: mockClient);
   });
 
-  group('getWeatherForCities', () {
+  group('getWeatherForCity', () {
     test('GIVEN a successful API call '
-        'WHEN getting weather for cities '
+        'WHEN getting weather for city '
         'THEN return WeatherResponse with correct data ', () async {
       final mockResponse = '''
         {
-          "cnt": 3,
-          "list": [
-            {
-              "coord": {"lon": -48.8456, "lat": -26.3044},
-              "sys": {"country": "BR", "timezone": -10800},
-              "weather": [{"id": 804, "main": "Clouds", "description": "overcast clouds", "icon": "04d"}],
-              "main": {"temp": 25.94, "pressure": 1012, "humidity": 97},
-              "visibility": 10000,
-              "wind": {"speed": 0.3, "deg": 236},
-              "clouds": {"all": 96},
-              "dt": 1743678548,
-              "id": 3459712,
-              "name": "Joinville"
-            }
-          ]
+          "location": {
+            "name": "Joinville",
+            "region": "Santa Catarina",
+            "country": "Brazil"
+          },
+          "current": {
+            "temp_c": 25.94,
+            "condition": {
+              "text": "Cloudy",
+              "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+              "code": 1003
+            },
+            "pressure_mb": 1012.0,
+            "pressure_in": 29.88,
+            "humidity": 97
+          },
+          "forecast": {
+            "forecastday": []
+          }
         }
       ''';
 
@@ -49,48 +52,56 @@ void main() {
         () => mockClient.get(any()),
       ).thenAnswer((_) async => http.Response(mockResponse, 200));
 
-      final result = await weatherService.getWeatherForCities([3459712]);
+      final result = await weatherService.getWeatherForCity(
+        cityName: 'Joinville',
+      );
 
       expect(result, isA<WeatherResponse>());
-      expect(result.count, equals(3));
-      expect(result.cities.length, equals(1));
-      expect(result.cities.first.name, equals('Joinville'));
-      expect(result.cities.first.country, equals('BR'));
-      expect(result.cities.first.weather.temp, equals(25.94));
-      expect(result.cities.first.weather.pressure, equals(1012));
-      expect(result.cities.first.weather.humidity, equals(97));
+      expect(result.location.name, equals('Joinville'));
+      expect(result.current.tempC, equals(25.94));
+      expect(result.current.pressureMb, equals(1012.0));
+      expect(result.current.humidity, equals(97));
     });
 
-    test('GIVEN an unsuccessful API call'
-        'WHEN getting weather for cities'
+    test('GIVEN an unsuccessful API call '
+        'WHEN getting weather for city '
         'THEN throw exception', () async {
       when(
         () => mockClient.get(any()),
       ).thenAnswer((_) async => http.Response('Not Found', 404));
 
       expect(
-        () => weatherService.getWeatherForCities([3459712]),
+        () => weatherService.getWeatherForCity(cityName: 'Joinville'),
         throwsException,
       );
     });
   });
 
-  group('getWeatherForCity', () {
-    test('GIVEN a successful API call'
-        'WHEN getting weather for a single city'
-        'THEN return CityWeather with correct data', () async {
+  group('getCurrentWeatherForCity', () {
+    test('GIVEN a successful API call '
+        'WHEN getting current weather for city '
+        'THEN return WeatherResponse with correct data', () async {
       final mockResponse = '''
         {
-          "coord": {"lon": -48.8456, "lat": -26.3044},
-          "sys": {"country": "BR", "timezone": -10800},
-          "weather": [{"id": 804, "main": "Clouds", "description": "overcast clouds", "icon": "04d"}],
-          "main": {"temp": 25.94, "pressure": 1012, "humidity": 97},
-          "visibility": 10000,
-          "wind": {"speed": 0.3, "deg": 236},
-          "clouds": {"all": 96},
-          "dt": 1743678548,
-          "id": 3459712,
-          "name": "Joinville"
+          "location": {
+            "name": "Joinville",
+            "region": "Santa Catarina", 
+            "country": "Brazil"
+          },
+          "current": {
+            "temp_c": 25.94,
+            "condition": {
+              "text": "Cloudy",
+              "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+              "code": 1003
+            },
+            "pressure_mb": 1012.0,
+            "pressure_in": 29.88,
+            "humidity": 97
+          },
+          "forecast": {
+            "forecastday": []
+          }
         }
       ''';
 
@@ -98,24 +109,28 @@ void main() {
         () => mockClient.get(any()),
       ).thenAnswer((_) async => http.Response(mockResponse, 200));
 
-      final result = await weatherService.getWeatherForCity(3459712);
+      final result = await weatherService.getCurrentWeatherForCity(
+        cityName: 'Joinville',
+      );
 
-      expect(result, isA<CityWeather>());
-      expect(result.name, equals('Joinville'));
-      expect(result.country, equals('BR'));
-      expect(result.weather.temp, equals(25.94));
-      expect(result.weather.pressure, equals(1012));
-      expect(result.weather.humidity, equals(97));
+      expect(result, isA<WeatherResponse>());
+      expect(result.location.name, equals('Joinville'));
+      expect(result.current.tempC, equals(25.94));
+      expect(result.current.pressureMb, equals(1012.0));
+      expect(result.current.humidity, equals(97));
     });
 
-    test('GIVEN an unsuccessful API call'
-        'WHEN getting weather for a single city'
+    test('GIVEN an unsuccessful API call '
+        'WHEN getting current weather for city '
         'THEN throw exception', () async {
       when(
         () => mockClient.get(any()),
       ).thenAnswer((_) async => http.Response('Not Found', 404));
 
-      expect(() => weatherService.getWeatherForCity(3459712), throwsException);
+      expect(
+        () => weatherService.getCurrentWeatherForCity(cityName: 'Joinville'),
+        throwsException,
+      );
     });
   });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:project_mark/src/domain/boundary/repositories/weather_repository.dart';
+import 'package:project_mark/src/presentation/controllers/weather_controller.dart';
+import 'package:project_mark/src/presentation/pages/weather_page.dart';
 
-import 'package:project_mark/main.dart';
+class MockWeatherRepository extends Mock implements IWeatherRepository {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  late WeatherController controller;
+  late MockWeatherRepository mockRepository;
+  final getIt = GetIt.instance;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setUp(() {
+    mockRepository = MockWeatherRepository();
+    controller = WeatherController(mockRepository);
+    getIt.registerSingleton<WeatherController>(controller);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  tearDown(() {
+    getIt.reset();
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('GIVEN initial state '
+      'WHEN rendering WeatherPage '
+      'THEN show initial content', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: WeatherPage()));
+
+    expect(find.byType(WeatherPage), findsOneWidget);
   });
 }
